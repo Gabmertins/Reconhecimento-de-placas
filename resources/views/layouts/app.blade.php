c3433270f9aa06053408d47522d00a68901c6629
Gabriel da Costa <gabmertins@gmail.com>
2019-10-02 19:43:52 -0300

    Removido script da app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -174,7 +174,6 @@
         </script>
         @stack('js')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
-        {!! $chart->script() !!}
 
     </body>
 </html>
